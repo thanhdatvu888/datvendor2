ca74f989c5ba09cfd0cfc3aec2af79b5fa9dc927
Thanh Dat <thanhdatvu888@gmail.com>
2018-10-07 18:17:24 +0700

    slldk

---
+++ src/Dat/ArrayCallback.php
@@ -0,0 +1,27 @@
+<?php
+namespace dat;
+class ArrayCallback {
+    public static function select($arr, $callback){
+        $result = [];
+        if($arr && is_array($arr)){
+            foreach($arr as $key=>$val){
+                $result[] = $callback($val, $key);
+            }
+        }
+        return $result;
+    }
+    
+    public static function reduce($arr, $callback, $inital=null){
+        $sum = $inital;
+        if($arr && is_array($arr)){
+            foreach($arr as $val){
+                $sum = $callback($sum, $val);
+            }
+        }
+        return $sum;
+    }
+
+
+}
+
+?>
